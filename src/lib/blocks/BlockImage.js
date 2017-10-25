334d362cc16c27fc0db9d444f9df7d4920b32332
liximomo <lixi@mufengcm.com>
2017-10-25 17:46:08 +0800

    new file

--- src/lib/blocks/BlockImage.js
+++ src/lib/blocks/BlockImage.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 import { EditorBlock, EditorState, SelectionState } from 'draft-js';
 
-import { getCurrentBlock } from '../../model/';
+import { getCurrentBlock } from '../operation/BLock';
 
 class BlockImage extends React.Component {
   static propTypes = {
@@ -11,23 +11,23 @@ class BlockImage extends React.Component {
     blockProps: PropTypes.object,
   };
 
-  focusBlock = () => {
-    const { block, blockProps } = this.props;
-    const { getEditorState, setEditorState } = blockProps;
-    const key = block.getKey();
-    const editorState = getEditorState();
-    const currentblock = getCurrentBlock(editorState);
-    if (currentblock.getKey() === key) {
-      return;
-    }
-    const newSelection = new SelectionState({
-      anchorKey: key,
-      focusKey: key,
-      anchorOffset: 0,
-      focusOffset: 0,
-    });
-    setEditorState(EditorState.forceSelection(editorState, newSelection));
-  };
+  // focusBlock = () => {
+  //   const { block, blockProps } = this.props;
+  //   const { getEditorState, setEditorState } = blockProps;
+  //   const key = block.getKey();
+  //   const editorState = getEditorState();
+  //   const currentblock = getCurrentBlock(editorState);
+  //   if (currentblock.getKey() === key) {
+  //     return;
+  //   }
+  //   const newSelection = new SelectionState({
+  //     anchorKey: key,
+  //     focusKey: key,
+  //     anchorOffset: 0,
+  //     focusOffset: 0,
+  //   });
+  //   setEditorState(EditorState.forceSelection(editorState, newSelection));
+  // };
 
   render() {
     const { block } = this.props;
@@ -35,14 +35,14 @@ class BlockImage extends React.Component {
     const src = data.get('src');
     if (src !== null) {
       return (
-        <div>
-          <div className="md-block-image-inner-container" onClick={this.focusBlock}>
+        <figure>
+          <div className="block-image-inner-container" onClick={this.focusBlock}>
             <img role="presentation" src={src} />
           </div>
           <figcaption>
             <EditorBlock {...this.props} />
           </figcaption>
-        </div>
+        </figure>
       );
     }
     return <EditorBlock {...this.props} />;
